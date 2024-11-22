4c8f9cead5cb4b60cffd395d985a269b54006e2b
yigit <yigituzuner2005@gmail.com>
2024-11-22 18:44:57 -0500

    refactor: better math

--- src/main/java/com/jonb/gaiusmod/effect/CreamyEffect.java
+++ src/main/java/com/jonb/gaiusmod/effect/CreamyEffect.java
@@ -15,7 +15,8 @@ public class CreamyEffect extends MobEffect {
     public boolean applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
         if(pLivingEntity.verticalCollisionBelow) {
             Vec3 initialVec = pLivingEntity.getDeltaMovement();
-            Vec3 pushVec = new Vec3(Math.random() * 0.1 - 0.039, initialVec.y, Math.random() * 0.1 - 0.039);
+            // Vec3 pushVec = new Vec3(Math.random() * 0.1 - 0.039, initialVec.y, Math.random() * 0.1 - 0.039);
+            Vec3 pushVec = new Vec3(-0.2 + Math.random() * (0.4), initialVec.y, -0.2 + Math.random() * (0.4));
             pLivingEntity.setDeltaMovement(pushVec);
             return true;
         }
